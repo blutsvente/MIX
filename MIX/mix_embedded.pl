4dde5225ab1af4161abadaddee8d56a75451ce94
abauer <abauer>
2004-07-08 11:21:49 +0000

    - added some wrapped functions

--- MIX/mix_embedded.pl
+++ MIX/mix_embedded.pl
@@ -13,12 +13,12 @@
 # +-----------------------------------------------------------------------+
 
 # +-----------------------------------------------------------------------+
-# | Id           : $Id: mix_embedded.pl,v 1.2 2004/07/06 14:56:18 abauer Exp $     |
+# | Id           : $Id: mix_embedded.pl,v 1.3 2004/07/08 11:21:49 abauer Exp $     |
 # | Name         : $Name:  $                                   |
 # | Description  : $Description: simple wrapper script for embedding MIX $|
 # | Parameters   : -                                                      | 
-# | Version      : $Revision: 1.2 $                                       |
-# | Mod.Date     : $Date: 2004/07/06 14:56:18 $                           |
+# | Version      : $Revision: 1.3 $                                       |
+# | Mod.Date     : $Date: 2004/07/08 11:21:49 $                           |
 # | Author       : $Author: abauer $                                      |
 # | Email        : $Email: Alexander.Bauer@micronas.com$                  |
 # |                                                                       |
@@ -87,7 +87,7 @@ use Micronas::MixWriter;
 # Global Variables
 #******************************************************************************
 
-$::VERSION = '$Revision: 1.2 $'; # RCS Id
+$::VERSION = '$Revision: 1.3 $'; # RCS Id
 $::VERSION =~ s,\$,,go;
 
 mix_init(); # load Presets ....
@@ -113,11 +113,14 @@ mix_getopt_header( qw(
     ));
 
 
+my ($r_connin, $r_ioin, $r_i2cin);
+
+
 sub readSpreadsheet(@) {
 
     my $input = shift;
 
-    my( $r_connin, $r_hierin, $r_ioin, $r_i2cin);
+    my $r_hierin;
     my $r_connmacros;
     my( $r_conngen, $r_hiergen);
 
@@ -143,22 +146,22 @@ sub readSpreadsheet(@) {
     purge_relicts();
 
     # Replace %MAC% before output
-    parse_mac();
+    #    parse_mac();
 
     # Add conections and ports if needed (hierachy traversal)
     # Add connections to TOPLEVEL for connections without ::in or ::out
     # Replace OPEN and %OPEN%
-    add_portsig();
+    #    add_portsig();
 
     # Replace %MAC% before output
     #!do before signal expansion ..... parse_mac();
 
     # Get rid of some "artefacts", again (add_portsig and add_sign2hier might have
     # added something ....
-    purge_relicts();
+    #    purge_relicts();
 
     # Add a list of all signals for each instance
-    add_sign2hier();
+    #    add_sign2hier();
 }
 
 #readSpreadsheet("/home/abauer/src/MIX/test/sxc_input/a_clk_i2c.sxc");
@@ -190,10 +193,17 @@ sub getStringFromEH($) {
     sub initTreeWalk()
     {
         # get root node
+	$location = $hierdb{'TESTBENCH'};
+	if(!defined($location))
+	{
+	    print("TESTBENCH not found!\n");
+	    return 0;
+	}
         $location = $hierdb{'TESTBENCH'}{'::treeobj'}->root;
 
 	if(!defined($location))
 	{
+	    print("NULL\n");
 	    return 0;
 	}
 	$stepInto = 1;
@@ -257,6 +267,94 @@ sub getInstLang($) {
     print "Instance: $name -> $temp\n";
 
     return $temp;
+}
+
+
+sub getNumConnRows() {
+    return (scalar $#{$r_connin});
+}
+
+
+sub getConnRow($) {
+
+    my $row = shift;
+
+    my @row = ();
+		       
+    push(@row, $r_connin->[$row]{'::ign'});
+    push(@row, $r_connin->[$row]{'::gen'});
+    push(@row, $r_connin->[$row]{'::bundle'});
+    push(@row, $r_connin->[$row]{'::class'});
+    push(@row, $r_connin->[$row]{'::clock'});
+    push(@row, $r_connin->[$row]{'::type'});
+    push(@row, $r_connin->[$row]{'::high'});
+    push(@row, $r_connin->[$row]{'::low'});
+    push(@row, $r_connin->[$row]{'::mode'});
+    push(@row, $r_connin->[$row]{'::name'});
+    push(@row, $r_connin->[$row]{'::out'});
+    push(@row, $r_connin->[$row]{'::in'});
+    push(@row, $r_connin->[$row]{'::descr'});
+    push(@row, $r_connin->[$row]{'::comment'});
+
+    return(@row);
+}
+
+
+sub getNumIOPadRows() {
+    return (scalar $#{$r_ioin});
+}
+
+
+sub getIOPadRow($) {
+
+    my $row = shift;
+
+    my @row = ();
+		       
+    push(@row, $r_ioin->[$row]{'::ign'});
+    push(@row, $r_ioin->[$row]{'::comment'});
+    push(@row, $r_ioin->[$row]{'::class'});
+    push(@row, $r_ioin->[$row]{'::ispin'});
+    push(@row, $r_ioin->[$row]{'::pin'});
+    push(@row, $r_ioin->[$row]{'::pad'});
+    push(@row, $r_ioin->[$row]{'::type'});
+    push(@row, $r_ioin->[$row]{'::iocell'});
+    push(@row, $r_ioin->[$row]{'::port'});
+    push(@row, $r_ioin->[$row]{'::name'});
+    push(@row, $r_ioin->[$row]{'::muxopt'}); # TODO: get all n-th muxopt
+
+    return(@row);
+}
+
+
+sub getNumI2CRows() {
+    return (scalar $#{$r_i2cin});
+}
+
+
+sub getI2CRow($) {
+
+    my $row = shift;
+
+    my @row = ();
+		       
+    push(@row, $r_i2cin->[$row]{'::ign'});
+    push(@row, $r_i2cin->[$row]{'::comment'});
+    push(@row, $r_i2cin->[$row]{'::variants'});
+    push(@row, $r_i2cin->[$row]{'::dev'});
+    push(@row, $r_i2cin->[$row]{'::sub'});
+    push(@row, $r_i2cin->[$row]{'::interface'});
+    push(@row, $r_i2cin->[$row]{'::block'});
+    push(@row, $r_i2cin->[$row]{'::dir'});
+    push(@row, $r_i2cin->[$row]{'::spec'});
+    push(@row, $r_i2cin->[$row]{'::clock'});
+    push(@row, $r_i2cin->[$row]{'::reset'});
+    push(@row, $r_i2cin->[$row]{'::busy'});
+    push(@row, $r_i2cin->[$row]{'::init'});
+    push(@row, $r_i2cin->[$row]{'::rec'});
+    push(@row, $r_i2cin->[$row]{'::b'}); # TODO: get all n-th b
+
+    return(@row);
 }
 
 
