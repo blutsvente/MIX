9847aee151087e4f48a7ac364ff395f652c37205
lutscher <lutscher>
2006-04-19 13:47:20 +0000

    added MSD parse on/off pragma for SVA in generated Verilog code

--- MIX/lib/perl/Micronas/RegViews.pm
+++ MIX/lib/perl/Micronas/RegViews.pm
@@ -1,8 +1,8 @@
 ###############################################################################
-#  RCSId: $Id: RegViews.pm,v 1.33 2006/04/19 12:58:30 lutscher Exp $
+#  RCSId: $Id: RegViews.pm,v 1.34 2006/04/19 13:47:20 lutscher Exp $
 ###############################################################################
 #
-#  Revision      : $Revision: 1.33 $                                  
+#  Revision      : $Revision: 1.34 $                                  
 #
 #  Related Files :  Reg.pm
 #
@@ -30,6 +30,9 @@
 ###############################################################################
 #
 #  $Log: RegViews.pm,v $
+#  Revision 1.34  2006/04/19 13:47:20  lutscher
+#  added MSD parse on/off pragma for SVA in generated Verilog code
+#
 #  Revision 1.33  2006/04/19 12:58:30  lutscher
 #  fixed bug in trans_done generation in Verilog code
 #
@@ -185,8 +188,10 @@ sub _gen_view_vgch_rs {
 				  'embedded_reg_name'  => "RS_CTLSTS",  # reserved name of special register embedded in ocp_target
 				  'field_spec_values'  => ['sha', 'w1c', 'usr'], # recognized values for spec attribute
 				  'indent'             => "    ",       # indentation character(s)
-				  'assert_pragma_start'=> "`ifdef ASSERT_ON",
-				  'assert_pragma_end'  => "`endif",
+				  'assert_pragma_start'=> "`ifdef ASSERT_ON
+// pragma msd parse off",
+				  'assert_pragma_end'  => "// pragma msd parse on
+`endif",
 				  # internal static data structs
 				  'hclocks'            => {},           # for storing per-clock-domain information
 				  'hfnames'            => {},           # for storing field names
@@ -593,8 +598,8 @@ endproperty
 	# add comment and pragmas to checking code and indent it (unless there is no code or not enabled)
 	if (scalar(@lchecks)) {
 		if ($this->global->{'infer_sva'}) {
-			unshift @lchecks, ("", "/*","  checking code","*/", $this->global->{'assert_pragma_start'});
-			push @lchecks, $this->global->{'assert_pragma_end'};
+			unshift @lchecks, ("", "/*","  checking code","*/", split("\n",$this->global->{'assert_pragma_start'}));
+			push @lchecks, split("\n",$this->global->{'assert_pragma_end'});
 			_pad_column(-1, $this->global->{'indent'}, 2, \@lchecks);
 		} else {
 			@lchecks=();
@@ -614,7 +619,7 @@ sub _vgch_rs_gen_udc_header {
 	my $pkg_name = $this;
 	$pkg_name =~ s/=.*$//;
 	push @$lref_res, ("/*", "  Generator information:", "  used package $pkg_name is version " . $this->global->{'version'});
-	my $rev = '  this package RegViews.pm is version $Revision: 1.33 $ ';
+	my $rev = '  this package RegViews.pm is version $Revision: 1.34 $ ';
 	$rev =~ s/\$//g;
 	$rev =~ s/Revision\: //;
 	push @$lref_res, $rev;
