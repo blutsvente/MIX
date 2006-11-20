ca9d8b39163fa9257eca1ed093cf8b777a1ade44
lutscher <lutscher>
2006-11-20 17:12:19 +0000

    major changes

--- MIX/lib/perl/Micronas/RegViewE.pm
+++ MIX/lib/perl/Micronas/RegViewE.pm
@@ -1,5 +1,5 @@
 ###############################################################################
-#  RCSId: $Id: RegViewE.pm,v 1.16 2006/11/13 17:24:42 lutscher Exp $
+#  RCSId: $Id: RegViewE.pm,v 1.17 2006/11/20 17:12:19 lutscher Exp $
 ###############################################################################
 #                                  
 #  Related Files :  Reg.pm
@@ -29,6 +29,9 @@
 ###############################################################################
 #
 #  $Log: RegViewE.pm,v $
+#  Revision 1.17  2006/11/20 17:12:19  lutscher
+#  major changes
+#
 #  Revision 1.16  2006/11/13 17:24:42  lutscher
 #  added reg_shell.e_vr_ad parameters
 #
@@ -136,8 +139,6 @@ use Micronas::MixUtils::RegUtils;
 # one output is generated containing all register space domains in the Reg object
 
 sub _gen_view_vr_ad {
-    my $holes_permission = "STANDARD"; # "STANDARD" or "AUTO" || STANDARD permission of holes = "R", 
-    # "AUTO"  permission of holes like the most in the register
 	my $this = shift;
 	my @ldomains;
 	my $href;
@@ -151,7 +152,8 @@ sub _gen_view_vr_ad {
                   'suffix'      => ".e",
                   'reg_macro'	=> "reg_def",
                   'field_macro'	=> "reg_fld",
-                  'hole_name'	=> "reserved_at_"
+                  'hole_name'	=> "reserved_at_",
+                  'hole_access' => "STANDARD" # "STANDARD" or "AUTO" || STANDARD permission of holes = "R", "AUTO"  permission of holes like the most in the register
 				 );
     
     # import regshell.<par> parameters from MIX package to class data; user can change these parameters in mix.cfg
@@ -159,7 +161,9 @@ sub _gen_view_vr_ad {
 	my @lmixparams = (
                       'reg_shell.e_vr_ad.regfile_prefix',
                       'reg_shell.e_vr_ad.file_prefix',
-                      'reg_shell.e_vr_ad.vplan_ref'
+                      'reg_shell.e_vr_ad.vplan_ref',    # if not %EMPTY%, will add coverage group with vplan_ref attribute
+                      'reg_shell.e_vr_ad.field_naming', # see Globals.pm
+                      'reg_shell.e_vr_ad.reg_naming'    # see Globals.pm
 					 );
     
     # make shortcuts for parameters
@@ -195,8 +199,6 @@ sub _gen_view_vr_ad {
 		_warning("no domains found to process, exiting");
 	};
     
-	my $my_reg_file_name = uc(join("_",$this->global->{regfile_prefix}, map {$_->{name}} @ldomains));
-    
 	my $e_filename = join("_",$this->global->{'file_prefix'}, map {$_->{name}} @ldomains).$this->global->{'suffix'};
 	open(E_FILE, ">$e_filename") || logwarn("ERROR: could not open file \'$e_filename\' for writing");
 	_info("generating file \'$e_filename\'");
@@ -205,24 +207,16 @@ sub _gen_view_vr_ad {
     
 	my ($top_inst, $o_field, $o_reg, $cov);
 	
-	# iterate through domains
-    
-	my %def = ();
-	my $aa;
-	my $bb;
-	my $ee;
-    
+	# iterate through domains   
 	foreach $o_domain (@ldomains) {
-	    foreach $o_reg (@{$o_domain->regs}) {
-            $def{$o_reg->{'definition'}}{definition} ='';	
-	    };
-	};
-    
-	foreach $o_domain (@ldomains) {
-        
-		# iterate through all registers of the domain and add ports/instantiations
-		foreach $o_reg (@{$o_domain->regs}) {
-            
+        my $domain = $o_domain->name;
+        my $domain_str = $this->global->{regfile_prefix}. "_". uc($domain);
+        my %hdef = ();
+        my $reg_name;   
+        my $reg_addr_str; 
+        #print E_FILE "\n-- domain $domain_str\n\n";
+		# iterate through all registers of the domain
+		foreach $o_reg (@{$o_domain->regs}) {            
 			my $reg_access = "";
 			my $reg_offset = $o_domain->get_reg_address($o_reg);
 			my @thefields = ();
@@ -236,33 +230,30 @@ sub _gen_view_vr_ad {
 			my %permission = ();
 			my $perm;
 			my $value;
-            
+            my $reg_access_mode = $o_reg->get_reg_access_mode(); # W, R or RW
 			$permission{"RW"} = 0;
 			$permission{"R"} = 0;
 			$permission{"W"} = 0;
-            
+             
+            $reg_addr_str = "0x" . _val2hex($eh->get('reg_shell.addrwidth'), $reg_offset);
 			if ($o_reg->{'definition'} eq '') {
-                # convert register name to upper case because it is an enum type in e
-			    print E_FILE $this->global->{reg_macro}, " ", uc($o_reg->name);
-			    print E_FILE " ", $this->global->{regfile_prefix}, "_";
-			};
+                $reg_name = _clone_name($this->global->{'reg_naming'}, 0, 0, $domain, $o_reg->name);     
+			} else {
+                $reg_name = $o_reg->{'definition'}; 
+                push @{$hdef{$reg_name}}, $reg_addr_str;
+            };
 			$ii =0;
-			
-		    
-			
+            
 			foreach $href (@{$o_reg->fields}) {
 			    $o_field = $href->{'field'};
-			    next if $o_field->name =~ m/^UPD[EF]/; # skip UPD* regs
-			    
-			    $thefields[$ii]{name} 		  = lc($o_field->name);
+			    next if $o_field->name =~ m/^UPD[EF]/; # skip UPD* regs		    
+			    $thefields[$ii]{name} 		  = _clone_name($this->global->{'field_naming'}, 0, 0, $domain, $reg_name, $o_field->name);
 			    $thefields[$ii]{pos}  		  = $href->{'pos'};
 			    $thefields[$ii]{size} 		  = $o_field->attribs->{'size'};
 			    $thefields[$ii]{rw}   		  = uc($o_field->attribs->{'dir'});
-			    $thefields[$ii]{parent_block} = $o_domain->name;
 			    $thefields[$ii]{init}         = "0x"._val2hex($o_field->attribs->{'size'}, $o_field->attribs->{'init'});
 			    $permission{$thefields[$ii]{rw}}++;
-			    $ii += 1;
-                
+			    $ii += 1;               
 			};
 			
 			@thefields = reverse sort {$a->{'pos'} <=> $b->{'pos'}} @thefields;
@@ -281,28 +272,26 @@ sub _gen_view_vr_ad {
                     $theholes[$ii]{pos}  = $singlefield->{pos} + $singlefield->{size};
                     $theholes[$ii]{name} = $hole_name . $theholes[$ii]{pos};
                     $theholes[$ii]{size} = $upper - ($singlefield->{pos} + $singlefield->{size});
-                    if ($holes_permission eq "STANDARD"){
+                    if ($this->global->{'hole_access'} eq "STANDARD"){
                         $theholes[$ii]{rw}   = "R";
                     }else{
                         $theholes[$ii]{rw}   = $perm;
                     };
-                    $theholes[$ii]{parent_block}   = $singlefield->{parent_block};
                     $theholes[$ii]{init}  = "0x0";
                     $ii++;
                 } 
-                $upper     = $singlefield->{pos};
+                $upper  = $singlefield->{pos};
             };
             
             if ($upper != 0) {
                 $theholes[$ii]{pos}  = 0;
                 $theholes[$ii]{name} = $hole_name . "0";
                 $theholes[$ii]{size} = $upper;
-                if ($holes_permission eq "STANDARD"){
+                if ($this->global->{'hole_access'} eq "STANDARD"){
                     $theholes[$ii]{rw}   = "R";
                 }else{
                     $theholes[$ii]{rw}   = $perm;
                 };
-                $theholes[$ii]{parent_block}   = "na";
                 $theholes[$ii]{init}  = "0x0";
                 $ii++;
             } 
@@ -310,14 +299,14 @@ sub _gen_view_vr_ad {
             @thefields_and_theholes = (@thefields,@theholes);
             @thefields_and_theholes = reverse sort {$a->{pos} <=> $b->{pos}} @thefields_and_theholes;
             
-            $ii =0;
-            $bb =0;
-            $aa = 0;
-            foreach $singlefield (@thefields_and_theholes) {
+            if ($o_reg->{'definition'} eq '' || scalar(@{$hdef{$reg_name}})==1) {
+                print E_FILE $this->global->{reg_macro}, " $reg_name";
                 if ($o_reg->{'definition'} eq '') {
-                    if ($ii == 0) {
-                        print E_FILE uc($singlefield->{parent_block}), " 0x", _val2hex($eh->get('reg_shell.addrwidth'), $reg_offset), " {\n";
-                    };
+                    print E_FILE " $domain_str $reg_addr_str \{\n";
+                } else {
+                    print E_FILE " \{\n";
+                };                
+                foreach $singlefield (@thefields_and_theholes) {
                     if ($singlefield->{name} =~ m/$hole_name/i) {
                         $cov = "";
                     } else {
@@ -327,76 +316,32 @@ sub _gen_view_vr_ad {
    @<<<<<< @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<: uint(bits:@>) : @< : @<<<<<<<<< @<<<<<< ; -- lsb position @>> 
 $this->global->{field_macro},$singlefield->{name},$singlefield->{size},$singlefield->{rw},$singlefield->{init},$cov, $singlefield->{pos} 
 .
-                    write E_FILE ;
-                    $ii += 1;	
-                }else{  
-                    if ($def{$o_reg->{'definition'}}{definition} eq '') {
-                        if ($singlefield->{name} =~ m/$hole_name/i) {
-                            $cov = "";
-                        } else {
-                            $cov = " : cov";
-                        };
-                        # reg - save
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{name} = $singlefield->{name};
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{size} = $singlefield->{size};
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{rw} = $singlefield->{rw};
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{init} = $singlefield->{init};
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{cov} = $cov;
-                        $def{$o_reg->{'definition'}}{fields}[$bb]{pos} = $singlefield->{pos};
-                        $bb += 1;    
-                    };
-                    if ($singlefield->{pos} == 0){
-                        # save every instance of reg with address
-                        foreach $ee(0..$#{ $def{$o_reg->{'definition'}}{list} }){
-                            $aa = $ee + 1;
-                        };
-                        $def{$o_reg->{'definition'}}{definition} = $o_reg->{'definition'};
-                        $def{$o_reg->{'definition'}}{list}[$aa]{MY_REG} = $o_reg->{'definition'};
-                        $def{$o_reg->{'definition'}}{list}[$aa]{MY_REG_FILE} =  $this->global->{regfile_prefix} . "_" . uc($singlefield->{parent_block});
-                        $def{$o_reg->{'definition'}}{list}[$aa]{address} = "0x" . _val2hex($eh->get('reg_shell.addrwidth'), $reg_offset);
-                    };
-                }; 
-            };
-            if ($o_reg->{'definition'} eq '') {
-                print E_FILE "};\n"; 
-                $this->write_extend_coverage(uc($o_reg->name));
+                    write E_FILE;
+                };
+                print E_FILE "};\n";
+                $this->write_extend_coverage($reg_name, $reg_access_mode); 
             };
         };
-    };
-    
-    my $cc;
-    my $dd;
-    my $valus;
-    
-    foreach $cc (keys %def){
-        if ($cc ne ''){
-            # output: regdef
-            print E_FILE $this->global->{reg_macro}, " ", $def{$cc}{list}[0]{MY_REG}, " {\n";
-            for $dd(0..$#{ $def{$cc}{fields} } ) {
-                printf E_FILE ("   %-7s %-33s : uint(bits:%2s) ", $this->global->{field_macro},$def{$cc}{fields}[$dd]{name}, $def{$cc}{fields}[$dd]{size});
-                printf E_FILE (": %-2s : %-10s %-7s ;", $def{$cc}{fields}[$dd]{rw}, $def{$cc}{fields}[$dd]{init},$def{$cc}{fields}[$dd]{cov});
-                printf E_FILE (" -- lsb position %3s\n", $def{$cc}{fields}[$dd]{pos});
-            };
-            print E_FILE "}; \n";
-            $this->write_extend_coverage($cc);
+        
+        my ($index, $n);
+        foreach $reg_name (keys %hdef){
+            $n = scalar(@{$hdef{$reg_name}});
             # output: list of regs instances
-            printf  E_FILE ("extend %s vr_ad_reg_file {\n",$def{$cc}{list}[0]{MY_REG_FILE}); 
-            foreach $ee(0..$#{ $def{$cc}{list} } ){
-                $valus = $ee;
-            };  
-            printf E_FILE ("  %%%s_n[%s]:list of %s vr_ad_reg;\n",lc($def{$cc}{list}[0]{MY_REG}),($valus+1),$def{$cc}{list}[0]{MY_REG});
+            printf E_FILE ("extend $domain_str vr_ad_reg_file {\n"); 
+            printf E_FILE ("  %%%s_n[$n]:list of $reg_name vr_ad_reg;\n",lc($reg_name));
             printf E_FILE ("  post_generate() is also {\n");
-            for $dd(0..$#{ $def{$cc}{list} } ) {
-                printf E_FILE ("    add_with_offset(%s,%s_n[%s]);\n",$def{$cc}{list}[$dd]{address},lc($def{$cc}{list}[0]{MY_REG}),$dd);
+            $index = 0;
+            foreach $reg_addr_str (@{$hdef{$reg_name}}) {
+                printf E_FILE ("    add_with_offset(%s,%s_n[%s]);\n",$reg_addr_str,lc($reg_name),$index);
+                $index++;
             };
-            _info("Generated a list of ", $def{$cc}{list}[0]{MY_REG}, " with ", ($valus+1), " items");
+            _info("generated a list of \'$reg_name\' vr_ad_reg with $n items");
             printf E_FILE ("  };\n");
             printf E_FILE ("};\n");
         };
+        printf E_FILE ("\n\nextend vr_ad_reg_file_kind : [%s];\n", $domain_str );
     };
     
-    
-    printf E_FILE ("\n\nextend vr_ad_reg_file_kind : [%s];\n", $my_reg_file_name );
     print E_FILE $this->global->{'footer'};
     
     close(E_FILE);
@@ -406,11 +351,25 @@ $this->global->{field_macro},$singlefield->{name},$singlefield->{size},$singlefi
 };
 
 sub write_extend_coverage {
-    my ($this, $name) = @_;
+    my ($this, $reg_name, $reg_access_mode) = @_;
+    my $ignore;
     if ($this->global->{'vplan_ref'} !~ m/%empty%/i) {
-        print E_FILE "extend ",uc($name), " vr_ad_reg {\n";
-        print E_FILE "  cover reg_access (kind == ",uc($name),") using also vplan_ref = \"", $this->global->{'vplan_ref'},"\";\n";
-        print E_FILE "};\n\n";
+        print E_FILE "extend $reg_name vr_ad_reg {\n";
+        print E_FILE "  cover reg_access (kind == $reg_name) using also vplan_ref = \"", $this->global->{'vplan_ref'},"\";\n";
+        print E_FILE "};\n";
+        if ($reg_access_mode ne "RW") {
+            if ($reg_access_mode eq "W") {
+                $ignore = "READ";
+            } else {
+                $ignore = "WRITE";
+            };
+            print E_FILE "extend vr_ad_reg {\n";
+            print E_FILE "  cover reg_access (kind == $reg_name) is also {\n";
+            print E_FILE "    item direction using also ignore = direction == ${ignore};\n";
+            print E_FILE "  };\n";
+            print E_FILE "};\n";
+        };
+        print E_FILE "\n";
     };
 };
 
